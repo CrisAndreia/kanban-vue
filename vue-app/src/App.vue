5ed6c49d9fb61ea1da979a6a30221cca08c0c806
Cristiane Andreia Pereira <cristiane.pereira@gesplan.com.br>
2022-05-09 11:01:40 +0100

    my_kanban_application

---
+++ vue-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class id="app">
+    <div class="container grid-lg">
+      <header class="navbar">
+        <section class="navbar-section">
+          <router-link to="/" class="btn btn-link">Home</router-link>
+          <router-link to="/kanban" class="btn btn-link">Kanban</router-link>
+        </section>
+        <section class="navbar-center">
+          <img class="img-responsive img-logo" src="@/assets/logo.png" alt="Logo da Switch Case" />
+        </section>
+        <section class="navbar-section">
+          <a href="https://www.linkedin.com/in/cristiane-andreia-pereira-12a182b0/" target="_blank" class="btn btn-link">Linkedin</a>
+          <a href="https://github.com/CrisAndreia" target="_blank" class="btn btn-link">GitHub</a>
+        </section>
+      </header>
+      <router-view class="pt-2" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.img-logo {
+  max-width: 100px;
+}
+</style>
+
+
+
---
+++ vue-app/src/components/MyFooter.vue
@@ -0,0 +1,53 @@
+<template>
+    <footer>
+        <a class="socialLink" v-for="(socialLink,index) in socialLinks" :key="index" :href="socialLink.link" target="blank">
+            <img :alt="socialLink.label" :title="socialLink.label" :src="socialLink.icon">
+        </a>
+    </footer>
+</template>
+
+<script>
+    import api from '@/services/api.js'
+
+    export default{
+        name: 'MyFooter',
+        data(){
+            return{
+                socialLinks: []
+            }
+        },
+        mounted(){
+            api.get('/social-links.json').then(response => {
+                this.socialLinks = response.data;
+                console.log(response.data);
+            })
+        }
+    }
+</script>
+
+<style scope>
+    footer{
+        width: 100%;
+        height: 60px;
+        background-color: var(--color-background-nav);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .socialLink{
+        margin: 0 10px;
+        border: 2px solid var(--color-text-light);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+    }
+
+    .socialLink img{
+        width: 20px;
+    }
+</style>
+
